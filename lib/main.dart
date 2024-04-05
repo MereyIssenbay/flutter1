d239614a8acec33086703318c3749e00b8c04b7b
Merey <misenbai@gmail.com>
2024-04-05 13:58:09 +0500

    first-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_villains/villains.dart';
-import 'package:wave/config.dart';
 import 'package:wave/wave.dart';
+import 'package:wave/config.dart';
+import 'package:flutter_villains/villains/villains.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,83 +11,87 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Assignment',
+      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MainScreen(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Main Screen'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Villain(
-              villainAnimation: VillainAnimation.fromBottom(
-                relativeOffset: 0.4,
-                from: Duration(milliseconds: 100),
-                to: Duration(milliseconds: 500),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) => SecondPage()),
+          );
+        },
+        child: Villain(
+          villainAnimation: VillainAnimation.fade(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Waves!',
+                style: TextStyle(fontSize: 24),
               ),
-              child: Container(
-                height: 200,
-                width: double.infinity,
-                color: Colors.blue,
-                child: Center(
-                  child: Text(
-                    'Your content here',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 24,
-                    ),
+              SizedBox(height: 20),
+              Villain(
+                villainAnimation: VillainAnimation.fromBottom(
+                  relativeOffset: 0.4,
+                  from: Duration(milliseconds: 200),
+                  to: Duration(milliseconds: 800),
+                ),
+                child: WaveWidget(
+                  config: CustomConfig(
+                    gradients: [
+                      [Colors.red, Colors.pinkAccent],
+                      [Colors.redAccent, Colors.deepOrange],
+                      [Colors.orange, Colors.yellowAccent]
+                    ],
+                    durations: [6000, 4000, 3000],
+                    heightPercentages: [0.20, 0.23, 0.26, 0.30],
+                    blur: MaskFilter.blur(BlurStyle.solid, 10),
+                    gradientBegin: Alignment.bottomLeft,
+                    gradientEnd: Alignment.topRight,
                   ),
+                  backgroundColor: Colors.white,
+                  size: Size(double.infinity, 500),
+                  waveAmplitude: 50,
                 ),
               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Next Page'),
+      ),
+      body: Villain(
+        villainAnimation: VillainAnimation.fade(),
+        child: Container(
+          color: Colors.white,
+          child: Center(
+            child: Text(
+              'Wlcome to the my first flutter project',
+              style: TextStyle(fontSize: 20),
             ),
-            SizedBox(height: 20),
-            Villain(
-              villainAnimation: VillainAnimation.fade(
-                from: Duration(milliseconds: 200),
-                to: Duration(milliseconds: 800),
-              ),
-              child: WaveWidget(
-                config: CustomConfig(
-                  gradients: [
-                    [Colors.blue, Colors.blue.shade200],
-                    [Colors.blue.shade200, Colors.blue.shade100],
-                  ],
-                  durations: [35000, 19440],
-                  heightPercentages: [0.20, 0.25],
-                  blur: MaskFilter.blur(BlurStyle.solid, 10),
-                  gradientBegin: Alignment.topLeft,
-                  gradientEnd: Alignment.bottomRight,
-                ),
-                size: Size(
-                  double.infinity,
-                  100.0,
-                ),
-                waveAmplitude: 20,
-                backgroundColor: Colors.white,
-                waveFrequency: 2,
-                heightPercentages: [0.25, 0.30],
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to another screen or perform an action
-              },
-              child: Text('Navigate'),
-            ),
-          ],
+          ),
         ),
       ),
     );
