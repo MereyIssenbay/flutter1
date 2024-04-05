394165cc45e6cc8828d4f386d086dcb6aa3e2505
Merey <misenbai@gmail.com>
2024-04-05 12:18:45 +0500

    first-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:wave/wave.dart';
+import 'package:flutter_villains/villains.dart';
 import 'package:wave/config.dart';
-import 'package:flutter_villains/villains/villains.dart';
-
+import 'package:wave/wave.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Interface Preferences',
+      title: 'Flutter Assignment',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MainScreen(),
     );
@@ -28,54 +28,67 @@ class MainScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Main Screen'),
       ),
-      body: Container(
+      body: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            WaveWidget(
-              config: CustomConfig(
-                gradients: [
-                  [Colors.blue, Colors.blue.shade200],
-                  [Colors.blue.shade200, Colors.blue.shade100],
-                ],
-                durations: [19440, 10800],
-                heightPercentages: [0.20, 0.25],
-                blur: MaskFilter.blur(BlurStyle.solid, 10),
-                gradientBegin: Alignment.bottomLeft,
-                gradientEnd: Alignment.topRight,
+            Villain(
+              villainAnimation: VillainAnimation.fromBottom(
+                relativeOffset: 0.4,
+                from: Duration(milliseconds: 100),
+                to: Duration(milliseconds: 500),
               ),
-              waveAmplitude: 0,
-              size: Size(
-                double.infinity,
-                double.infinity,
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                color: Colors.blue,
+                child: Center(
+                  child: Text(
+                    'Your content here',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
               ),
             ),
+            SizedBox(height: 20),
+            Villain(
+              villainAnimation: VillainAnimation.fade(
+                from: Duration(milliseconds: 200),
+                to: Duration(milliseconds: 800),
+              ),
+              child: WaveWidget(
+                config: CustomConfig(
+                  gradients: [
+                    [Colors.blue, Colors.blue.shade200],
+                    [Colors.blue.shade200, Colors.blue.shade100],
+                  ],
+                  durations: [35000, 19440],
+                  heightPercentages: [0.20, 0.25],
+                  blur: MaskFilter.blur(BlurStyle.solid, 10),
+                  gradientBegin: Alignment.topLeft,
+                  gradientEnd: Alignment.bottomRight,
+                ),
+                size: Size(
+                  double.infinity,
+                  100.0,
+                ),
+                waveAmplitude: 20,
+                backgroundColor: Colors.white,
+                waveFrequency: 2,
+                heightPercentages: [0.25, 0.30],
+              ),
+            ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                VillainController.fade(
-                  context,
-                  child: NextScreen(),
-                );
+                // Navigate to another screen or perform an action
               },
-              child: Text('Go to Next Screen'),
+              child: Text('Navigate'),
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class NextScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Next Screen'),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Text('You\'ve transitioned to the next screen!'),
       ),
     );
   }
